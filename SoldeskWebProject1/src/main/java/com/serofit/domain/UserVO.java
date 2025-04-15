4aa8b0fb24c32130d7dfbcca9912454b87b97c6f
yangMinjae <ymj137955@naver.com>
2025-04-15 16:40:32 +0900

    Created User, MateVO

--- SoldeskWebProject1/src/main/java/com/serofit/domain/UserVO.java
+++ SoldeskWebProject1/src/main/java/com/serofit/domain/UserVO.java
@@ -1,5 +1,16 @@
 package com.serofit.domain;
 
-public class UserVO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Data
+public class UserVO {
+	private int uno;
+	private String id;
+	private String pw;
+	private String nickname;
+	private String email;
 }
