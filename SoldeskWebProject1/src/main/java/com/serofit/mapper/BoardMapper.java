976452e748dce6f18e90069a0a4199ade0f46505
yangMinjae <ymj137955@naver.com>
2025-04-15 17:49:30 +0900

    BoardMapper 90percent completed

---
+++ SoldeskWebProject1/src/main/java/com/serofit/mapper/BoardMapper.java
@@ -0,0 +1,32 @@
+package com.serofit.mapper;
+
+
+import java.util.List;
+
+import com.serofit.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	// 게시글 불러오기
+	public BoardVO getPost();
+	
+	// 조회수 업데이트
+	public int updateHit();
+	
+	// 좋아요 업데이트
+	public int updateLike();
+	
+	// 인기 게시글 가져오기
+	public List<BoardVO> getHotPosts();
+	
+	// 게시글 목록 불러오기
+	public List<BoardVO> getPostList();
+	
+	// 게시글 작성
+	public int insertPost();
+	
+	// 게시글 삭제
+	public int deletePostByBno();
+	
+	// 회원탈퇴 게시글 삭제
+}
