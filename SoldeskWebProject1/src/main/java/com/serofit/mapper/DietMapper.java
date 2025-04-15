d8a196c5184482ad06bec21a94c4d9992e5dd279
배상민 <sfad7@naver.com>
2025-04-15 17:50:00 +0900

    DietMapper 완료

---
+++ SoldeskWebProject1/src/main/java/com/serofit/mapper/DietMapper.java
@@ -0,0 +1,18 @@
+package com.serofit.mapper;
+
+import java.util.List;
+
+import com.serofit.domain.DietVO;
+
+public interface DietMapper {
+	
+	// 식단 결과
+	public int insertDiet(DietVO dvo);
+	
+	// UNO로 식단 목록 불러오기
+	public List<DietVO> selectDietByUno(int uno);
+	
+	// UNO로 식단 삭제
+	public int deleteDietByUno(int uno);
+	
+}
