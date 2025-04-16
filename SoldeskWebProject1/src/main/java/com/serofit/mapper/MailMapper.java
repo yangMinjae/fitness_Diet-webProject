aafbacd44e11e124c3a7fdf6020d8da355e969d3
배상민 <sfad7@naver.com>
2025-04-16 16:37:53 +0900

    DietMapper 최종 수정, MailMapper 완료

---
+++ SoldeskWebProject1/src/main/java/com/serofit/mapper/MailMapper.java
@@ -0,0 +1,19 @@
+package com.serofit.mapper;
+
+import java.util.List;
+import com.serofit.domain.MailVO;
+
+public interface MailMapper {
+	
+	// 받은 사람 기준으로 메일 불러오기
+	public int insertMail(MailVO mvo);
+	
+	// 메일 보내기
+	public List<MailVO> selectByReceiver(MailVO mvo);
+	
+	// MNO로 메일 삭제
+	public int deleteMailByMno(int mno); 
+	
+	// 탈퇴 시 메일 전체 삭제
+	public int deleteMailByUno(MailVO mvo); 
+}
