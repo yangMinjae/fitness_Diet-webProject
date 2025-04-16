0d63d47ac30114a77c206bf0aa66eb10ab1a6847
yangMinjae <ymj137955@naver.com>
2025-04-16 16:21:18 +0900

    Finished BoardMapper

--- SoldeskWebProject1/src/main/java/com/serofit/mapper/BoardMapper.java
+++ SoldeskWebProject1/src/main/java/com/serofit/mapper/BoardMapper.java
@@ -3,18 +3,14 @@ package com.serofit.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.serofit.domain.BoardVO;
 
 public interface BoardMapper {
 	
 	// 게시글 불러오기
-	public BoardVO getPost();
-	
-	// 조회수 업데이트
-	public int updateHit();
-	
-	// 좋아요 업데이트
-	public int updateLike();
+	public BoardVO getPostByBno(int bno);
 	
 	// 인기 게시글 가져오기
 	public List<BoardVO> getHotPosts(int quantity);
@@ -22,12 +18,21 @@ public interface BoardMapper {
 	// 게시글 목록 불러오기
 	public List<BoardVO> getPostList();
 	
+
+	// 조회수 업데이트
+	public int updateHit(int bno);
+	
+	// 좋아요 업데이트
+	public int updateLike(@Param("bno") int bno, @Param("plOrMi") int plOrMi);	
+	// int plOrMi -> plusOrMinus - 서비스에서 1아니면 -1을 전달
+	
+	
 	// 게시글 작성
-	public int insertPost();
+	public int insertPost(@Param("bvo") BoardVO bvo, @Param("dno") int dno, @Param("uno") int uno);
 	
 	// 게시글 삭제
-	public int deletePostByBno();
+	public int deletePostByBno(int bno);
 	
 	// 회원탈퇴 게시글 삭제
-	public int deletePostByUno();
+	public int deletePostByUno(int uno);
 }
