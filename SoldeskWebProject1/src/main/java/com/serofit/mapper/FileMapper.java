69d533529c6604b8aa256c3d7f56b3f74e1ca6b5
jinsik <tom1110@naver.com>
2025-04-16 16:36:21 +0900

    FileMapper 2

--- SoldeskWebProject1/src/main/java/com/serofit/mapper/FileMapper.java
+++ SoldeskWebProject1/src/main/java/com/serofit/mapper/FileMapper.java
@@ -1,5 +1,7 @@
 package com.serofit.mapper;
 
+import java.util.List;
+
 import com.serofit.domain.FileVO;
 
 public interface FileMapper {
@@ -7,6 +9,15 @@ public interface FileMapper {
 	
 	// 이미지 업로드, 수정
 	public int insertFile(FileVO fvo);
+	// 프로필 사진 등록
+	public int insertProfileFile(FileVO fvo);
 	// 이미지 삭제
 	public int deleteFile(int bno);
+	// uuid 파일삭제
+	public String deleteByUuid(String uuid);
+	// 프로필 이미지 띄우기
+	public List<FileVO>selectUprofileFile(String uuid);
+	// 게시물에서 이미지 띄우기
+	public List<FileVO>selectBoardFile(int bno);
+
 }
