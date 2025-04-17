286aba215901361470104f108bca62c8cb6cabd6
yangMinjae <ymj137955@naver.com>
2025-04-17 09:37:01 +0900

    .

--- SoldeskWebProject1/src/test/java/com/serofit/mapper/BoardMapperTests.java
+++ SoldeskWebProject1/src/test/java/com/serofit/mapper/BoardMapperTests.java
@@ -1,8 +1,12 @@
 package com.serofit.mapper;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.serofit.domain.BoardVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -10,5 +14,13 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class BoardMapperTests {
-
+	@Autowired
+	BoardMapper bmapper;
+	
+	@Test
+	public void getPostByBnoTest() {
+		BoardVO bvo;
+		bvo = bmapper.getPostByBno(1);
+		log.info("getPostByBno..."+bvo);
+	}
 }
