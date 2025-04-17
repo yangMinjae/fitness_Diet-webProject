aab47f813d673d52567921460a77eb9f524bb384
배상민 <sfad7@naver.com>
2025-04-17 10:00:15 +0900

    DietMapperTest 완료

--- SoldeskWebProject1/src/test/java/com/serofit/mapper/DietMapperTests.java
+++ SoldeskWebProject1/src/test/java/com/serofit/mapper/DietMapperTests.java
@@ -1,5 +1,8 @@
 package com.serofit.mapper;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,18 +22,34 @@ public class DietMapperTests {
 	@Autowired
 	DietMapper dmapper;
 	
+	@Test
+	public void getInsertDiet() {
+		DietVO dvo = new DietVO();
+		
+		dvo.setUno(1);
+		dvo.setContent("테스트 컨텐츠");
+		dvo.setTag("테스트 태그");
+		dvo.setTitle("테스트 타이틀");
+		
+		int result = dmapper.insertDiet(dvo);
+		System.out.println("result : " + result);
+	}
+	
 //	@Test
-//	public void getInsertDiet() {
-//		DietVO dvo = new DietVO();
+//	public void getSelectDietByUno() {
+//		int uno = 1;
 //		
-//		dvo.setUno(1);
-//		dvo.setContent("테스트 컨텐츠");
-//		dvo.setTag("테스트 태그");
-//		dvo.setTitle("테스트 타이틀");
-//		
-//		int result = dmapper.insertDiet(dvo);
-//		System.out.println("result : " + result);
+//		List<DietVO> list = dmapper.selectDietByUno(uno);
+//		for (DietVO dvo : list) {
+//			System.out.println(dvo);
+//		}
 //	}
 	
-	
+//	@Test
+//	public void getDeleteDietByUno() {
+//		int uno = 1;
+//		
+//		int result = dmapper.deleteDietByUno(uno);
+//		System.out.println("result : " + result);
+//	}
 }
