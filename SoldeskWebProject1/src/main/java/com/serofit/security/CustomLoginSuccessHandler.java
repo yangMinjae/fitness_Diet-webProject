3ca8cfbc1bf71fae597b1e0ce6c653b864664cb1
JoSeongHyeon <superjaehyo@gamil.com>
2025-05-15 09:27:14 +0900

    로그인시 페이지 이동 완료

--- SoldeskWebProject1/src/main/java/com/serofit/security/CustomLoginSuccessHandler.java
+++ SoldeskWebProject1/src/main/java/com/serofit/security/CustomLoginSuccessHandler.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j;
@@ -15,10 +18,24 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+	
+	private final RequestCache requestCache = new HttpSessionRequestCache();
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		log.warn("Login Success");
-		response.sendRedirect("/");
+		 	log.warn("Login Success");
+
+	        // 사용자가 로그인 전에 접근했던 URL 가져오기
+	        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+	        if (savedRequest != null) {
+	            String redirectUrl = savedRequest.getRedirectUrl();
+	            log.warn("Redirecting to saved request URL: " + redirectUrl);
+	            response.sendRedirect(redirectUrl);
+	        } else {
+	            log.warn("Redirecting to default URL: /");
+	            response.sendRedirect("/");
+	        }
 	}
 }
