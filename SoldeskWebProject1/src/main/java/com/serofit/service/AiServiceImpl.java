5c783ed90423205bd3e0faf4149b959681ee2a31
yangMinjae <ymj137955@naver.com>
2025-07-01 16:43:26 +0900

    ai service 수정(불필요한 코드 제거)

--- SoldeskWebProject1/src/main/java/com/serofit/service/AiServiceImpl.java
+++ SoldeskWebProject1/src/main/java/com/serofit/service/AiServiceImpl.java
@@ -18,16 +18,22 @@ public class AiServiceImpl implements AiService{
 	@Override
 	public String getResult(String prompt) {
 		OpenAIClient client = OpenAIOkHttpClient.builder()
-				.fromEnv()
 				.apiKey(aiApiKey)
-				.baseUrl("https://api.openai.com/v1")
 				.build();
+		
 		ResponseCreateParams params = ResponseCreateParams.builder()
 		        .input(prompt)
 		        .model(ChatModel.CHATGPT_4O_LATEST)
 		        .build();
 		
 		Response response = client.responses().create(params);
-		return response.output().get(0).message().get().content().get(0).outputText().get().text();
+		return response.output().stream()
+				.findFirst()
+				.flatMap(o -> o.message())
+				.flatMap(m -> m.content().stream()
+					.findFirst()
+					.flatMap(c -> c.outputText().map(t -> t.text())))
+				.orElse("[No response]");
+		//return response.output().get(0).message().get().content().get(0).outputText().get().text();
 	}
 }
